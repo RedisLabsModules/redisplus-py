50876e023aa423729ac04697ec474cab1990c8ba
Chayim I. Kirshen <c@kirshen.com>
2021-07-19 14:02:35 +0300

    tox integration

--- redisplus/client.py
+++ redisplus/client.py
@@ -6,7 +6,7 @@ import importlib
 class RedisClient(object):
     """General client to be used for redis modules"""
 
-    def __init__(self, modules: Dict, client=Optional[Redis]):
+    def __init__(self, modules: Dict, client=Optional[Redis], from_url=Optional[str]):
         """
         Creates an all-purpose client and passes them in to
         all of the resulting modules.
@@ -18,14 +18,20 @@ class RedisClient(object):
 
         :param client: A pre-build client of type redis.Redis. This is used to
                        create all clients.
-        :type client: redis.Redis
+
+        :param from_url: A connection string, for building a redis client.
+        :type from_url: str
         """
         if client is not None:
             self.CLIENT = client
         else:
-            self.CLIENT = Redis()
+            if from_url is not None:
+                self.CLIENT = Redis.from_url(from_url)
+            else:
+                self.CLIENT = Redis()
 
         for key, vals in modules.items():
             modclient = "{}_CLIENT".format(key.upper())
             x = importlib.import_module(key)
+            vals['conn'] = x  # NOTE redisearch only, each module is different
             setattr(self, modclient, x.Client(**vals))
--- tests/test_client.py
+++ tests/test_client.py
@@ -2,9 +2,14 @@ from redisplus.client import RedisClient
 import contextlib
 import redis
 from utils import mockredisclient
+import pytest
 
-def test_init():
+@pytest.mark.redisearch
+def test_client_init():
     modules = {'redisearch': {'index_name': 'testtheindex'}}
+
+    with pytest.raises(TypeError):
+        RedisClient(from_url="redis://localhost:6379")
 
     # limit validations to our library interaction - not redis
     with contextlib.ExitStack() as stack:
@@ -17,4 +22,10 @@ def test_init():
         stack = mockredisclient(stack)
         # create our own client
         rc = RedisClient(modules=modules, client=redis.Redis(port=56789))
+        assert rc.CLIENT is not None
+
+    with contextlib.ExitStack() as stack:
+        stack = mockredisclient(stack)
+        # create our own client
+        rc = RedisClient(modules, from_url="redis://localhost:6379")
         assert rc.CLIENT is not None
