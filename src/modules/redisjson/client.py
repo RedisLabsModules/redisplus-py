d57139225570a8ff73e69f8fbc8ca8f31a1b9dd5
Chayim I. Kirshen <c@kirshen.com>
2021-08-04 10:32:16 +0300

    tests re-wrapped

--- src/modules/redisjson/client.py
+++ src/modules/redisjson/client.py
@@ -96,9 +96,8 @@ class Client(CommandMixin, CM, object):
             transaction=transaction,
             shard_hint=shard_hint
         )
-        p.__encoder__ = self.__encoder__ #.setEncoder(self._encoder)
-        p.__decoder__ = self.__decoder__ #.setEncoder(self._decoder)
-        # p.setDecoder(self._decoder)
+        p.__encoder__ = self.__encoder__
+        p.__decoder__ = self.__decoder__
         return p
 
     # commands = recmds
