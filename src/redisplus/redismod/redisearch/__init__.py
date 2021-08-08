0e28eba5ccd7793aadbac1e9c6991bf0593ac16b
AvitalFineRedis <avital.fine@redislabs.com>
2021-08-08 16:46:27 +0300

    add redisearch module

---
+++ src/redisplus/redismod/redisearch/__init__.py
@@ -0,0 +1,8 @@
+from .result import Result
+from .document import Document
+from .client import Client
+from .field import NumericField, TextField, GeoField, TagField
+from .indexDefinition import IndexDefinition
+from .query import Query, NumericFilter, GeoFilter, SortbyField
+from .aggregation import AggregateRequest, AggregateResult
+from .auto_complete import AutoCompleter, Suggestion
