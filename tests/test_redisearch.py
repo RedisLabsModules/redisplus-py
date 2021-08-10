d1515b25e0c7e3c83e72163de997d27c64a7f45e
Chayim I. Kirshen <c@kirshen.com>
2021-08-10 13:26:08 +0300

    now, tests run

--- tests/test_redisearch.py
+++ tests/test_redisearch.py
@@ -116,97 +116,96 @@ def test_base():
 @pytest.mark.integrations
 @pytest.mark.redisearch
 def testClient(client):
-    conn = redis()
 
-    with conn as r:
-        num_docs = 500
-        r.flushdb()
-        # createIndex(client.client, num_docs=num_docs)
-        for _ in r.retry_with_rdb_reload():
-            waitForIndex(r, 'test')
-            # verify info
-            info = client.info()
-            for k in ['index_name', 'index_options', 'attributes', 'num_docs',
-                      'max_doc_id', 'num_terms', 'num_records', 'inverted_sz_mb',
-                      'offset_vectors_sz_mb', 'doc_table_size_mb', 'key_table_size_mb',
-                      'records_per_doc_avg', 'bytes_per_record_avg', 'offsets_per_term_avg',
-                      'offset_bits_per_record_avg']:
-                assert k in info
+    num_docs = 500
+    r = client
+    r.flushdb()
+    # createIndex(client.client, num_docs=num_docs)
+    # for _ in r.retry_with_rdb_reload():
+    waitForIndex(r, 'test')
+    # verify info
+    info = client.info()
+    for k in ['index_name', 'index_options', 'attributes', 'num_docs',
+                'max_doc_id', 'num_terms', 'num_records', 'inverted_sz_mb',
+                'offset_vectors_sz_mb', 'doc_table_size_mb', 'key_table_size_mb',
+                'records_per_doc_avg', 'bytes_per_record_avg', 'offsets_per_term_avg',
+                'offset_bits_per_record_avg']:
+        assert k in info
 
-            assert client.index_name == info['index_name']
-            assert num_docs == int(info['num_docs'])
+    assert client.index_name == info['index_name']
+    assert num_docs == int(info['num_docs'])
 
-            res = client.search("henry iv")
-            assert isinstance(res, Result)
-            assert (225 == res.total)
-            assert (10 == len(res.docs))
-            assert (res.duration > 0)
+    res = client.search("henry iv")
+    assert isinstance(res, Result)
+    assert (225 == res.total)
+    assert (10 == len(res.docs))
+    assert (res.duration > 0)
 
-            for doc in res.docs:
-                assert doc.id
-                assert (doc.play == 'Henry IV')
-                assert (len(doc.txt) > 0)
+    for doc in res.docs:
+        assert doc.id
+        assert (doc.play == 'Henry IV')
+        assert (len(doc.txt) > 0)
 
-            # test no content
-            res = client.search(Query('king').no_content())
-            assert (194 == res.total)
-            assert (10 == len(res.docs))
-            for doc in res.docs:
-                assert ('txt' not in doc.__dict__)
-                assert ('play' not in doc.__dict__)
+    # test no content
+    res = client.search(Query('king').no_content())
+    assert (194 == res.total)
+    assert (10 == len(res.docs))
+    for doc in res.docs:
+        assert ('txt' not in doc.__dict__)
+        assert ('play' not in doc.__dict__)
 
-            # test verbatim vs no verbatim
-            total = client.search(Query('kings').no_content()).total
-            vtotal = client.search(Query('kings').no_content().verbatim()).total
-            assert (total > vtotal)
+    # test verbatim vs no verbatim
+    total = client.search(Query('kings').no_content()).total
+    vtotal = client.search(Query('kings').no_content().verbatim()).total
+    assert (total > vtotal)
 
-            # test in fields
-            txt_total = client.search(Query('henry').no_content().limit_fields('txt')).total
-            play_total = client.search(Query('henry').no_content().limit_fields('play')).total
-            both_total = client.search(Query('henry').no_content().limit_fields('play', 'txt')).total
-            assert (129 == txt_total)
-            assert (494 == play_total)
-            assert (494 == both_total)
+    # test in fields
+    txt_total = client.search(Query('henry').no_content().limit_fields('txt')).total
+    play_total = client.search(Query('henry').no_content().limit_fields('play')).total
+    both_total = client.search(Query('henry').no_content().limit_fields('play', 'txt')).total
+    assert (129 == txt_total)
+    assert (494 == play_total)
+    assert (494 == both_total)
 
-            # test load_document
-            doc = client.load_document('henry vi part 3:62')
-            assert (doc is not None)
-            assert ('henry vi part 3:62' == doc.id)
-            assert (doc.play == 'Henry VI Part 3')
-            assert (len(doc.txt) > 0)
+    # test load_document
+    doc = client.load_document('henry vi part 3:62')
+    assert (doc is not None)
+    assert ('henry vi part 3:62' == doc.id)
+    assert (doc.play == 'Henry VI Part 3')
+    assert (len(doc.txt) > 0)
 
-            # test in-keys
-            ids = [x.id for x in client.search(Query('henry')).docs]
-            assert (10 == len(ids))
-            subset = ids[:5]
-            docs = client.search(Query('henry').limit_ids(*subset))
-            assert (len(subset) == docs.total)
-            ids = [x.id for x in docs.docs]
-            assert (set(ids) == set(subset))
+    # test in-keys
+    ids = [x.id for x in client.search(Query('henry')).docs]
+    assert (10 == len(ids))
+    subset = ids[:5]
+    docs = client.search(Query('henry').limit_ids(*subset))
+    assert (len(subset) == docs.total)
+    ids = [x.id for x in docs.docs]
+    assert (set(ids) == set(subset))
 
-            # self.assertRaises(redis.ResponseError, client.search, Query('henry king').return_fields('play', 'nonexist'))
+    # self.assertRaises(redis.ResponseError, client.search, Query('henry king').return_fields('play', 'nonexist'))
 
-            # test slop and in order
-            assert (193 == client.search(Query('henry king')).total)
-            assert (3 == client.search(Query('henry king').slop(0).in_order()).total)
-            assert (52 == client.search(Query('king henry').slop(0).in_order()).total)
-            assert (53 == client.search(Query('henry king').slop(0)).total)
-            assert (167 == client.search(Query('henry king').slop(100)).total)
+    # test slop and in order
+    assert (193 == client.search(Query('henry king')).total)
+    assert (3 == client.search(Query('henry king').slop(0).in_order()).total)
+    assert (52 == client.search(Query('king henry').slop(0).in_order()).total)
+    assert (53 == client.search(Query('henry king').slop(0)).total)
+    assert (167 == client.search(Query('henry king').slop(100)).total)
 
-            # test delete document
-            client.add_document('doc-5ghs2', play='Death of a Salesman')
-            res = client.search(Query('death of a salesman'))
-            assert (1 == res.total)
+    # test delete document
+    client.add_document('doc-5ghs2', play='Death of a Salesman')
+    res = client.search(Query('death of a salesman'))
+    assert (1 == res.total)
 
-            assert (1 == client.delete_document('doc-5ghs2'))
-            res = client.search(Query('death of a salesman'))
-            assert (0 == res.total)
-            assert (0 == client.delete_document('doc-5ghs2'))
+    assert (1 == client.delete_document('doc-5ghs2'))
+    res = client.search(Query('death of a salesman'))
+    assert (0 == res.total)
+    assert (0 == client.delete_document('doc-5ghs2'))
 
-            client.add_document('doc-5ghs2', play='Death of a Salesman')
-            res = client.search(Query('death of a salesman'))
-            assert (1 == res.total)
-            client.delete_document('doc-5ghs2')
+    client.add_document('doc-5ghs2', play='Death of a Salesman')
+    res = client.search(Query('death of a salesman'))
+    assert (1 == res.total)
+    client.delete_document('doc-5ghs2')
 
 def testAddHash(client):
     conn = client.redis()
