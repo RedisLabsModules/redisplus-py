84e668e84c06920c3719a27a2b80d42d5fbf9e76
AvitalFineRedis <avital.fine@redislabs.com>
2021-08-10 11:03:39 +0300

    change base test

--- tests/test_redisbloom.py
+++ tests/test_redisbloom.py
@@ -1,14 +1,29 @@
 import pytest
 from redis import Redis
-from src.redisplus import RedisClient
+from redisplus.client import RedisClient
 
 @pytest.fixture
 def client():
     rc = RedisClient(modules={'redisbloom': {"client": Redis()}})
     rc.redisbloom.flushdb()
+
     return rc.redisbloom
 
 @pytest.mark.redisbloom
-def test_base():
-    # try not to break the regular client init
-    rc = RedisClient(modules={'redisbloom': {'client': Redis()}})
+def test_base(client):
+    rc = RedisClient(modules={'redisbloom': {"client": Redis()}})
+    rc.redisbloom.flushdb()
+
+def testCreate(client):
+    '''Test CREATE/RESERVE calls'''
+    assert (client.bfCreate('bloom', 0.01, 1000))
+    assert (client.bfCreate('bloom_e', 0.01, 1000, expansion=1))
+    assert (client.bfCreate('bloom_ns', 0.01, 1000, noScale=True))
+    assert (client.cfCreate('cuckoo', 1000))
+    assert (client.cfCreate('cuckoo_e', 1000, expansion=1))
+    assert (client.cfCreate('cuckoo_bs', 1000, bucket_size=4))
+    assert (client.cfCreate('cuckoo_mi', 1000, max_iterations=10))
+    assert (client.cmsInitByDim('cmsDim', 100, 5))
+    assert (client.cmsInitByProb('cmsProb', 0.01, 0.01))
+    assert (client.topkReserve('topk', 5, 100, 5, 0.9))
+    assert (client.tdigestCreate('tDigest', 100))
