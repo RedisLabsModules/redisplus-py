5dac8e757346313e5c9df916890e275460bf5120
AvitalFineRedis <avital.fine@redislabs.com>
2021-09-22 17:39:01 +0200

    json links

--- redisplus/json/commands.py
+++ redisplus/json/commands.py
@@ -6,7 +6,10 @@ class CommandMixin:
     """json commands."""
 
     def arrappend(self, name, path=Path.rootPath(), *args):
-        """Append the objects ``args`` to the array under the ``path` in key ``name``."""
+        """
+        Append the objects `args` to the array under the `path` in key `name`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonarrappend>`_.
+        """
         pieces = [name, str_path(path)]
         for o in args:
             pieces.append(self._encode(o))
@@ -14,61 +17,96 @@ class CommandMixin:
 
     def arrindex(self, name, path, scalar, start=0, stop=-1):
         """
-        Return the index of ``scalar`` in the JSON array under ``path`` at key ``name``.
-
-        The search can be limited using the optional inclusive ``start`` and exclusive ``stop`` indices.
+        Return the index of `scalar` in the JSON array under `path` at key `name`.
+        The search can be limited using the optional inclusive `start` and exclusive `stop` indices.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonarrindex>`_.
         """
         return self.execute_command(
             "JSON.ARRINDEX", name, str_path(path), self._encode(scalar), start, stop
         )
 
     def arrinsert(self, name, path, index, *args):
-        """Insert the objects ``args`` to the array at index ``index`` under the ``path` in key ``name``."""
+        """
+        Insert the objects `args` to the array at index `index` under the `path` in key `name`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonarrinsert>`_.
+        """
         pieces = [name, str_path(path), index]
         for o in args:
             pieces.append(self._encode(o))
         return self.execute_command("JSON.ARRINSERT", *pieces)
 
     def forget(self, name, path=Path.rootPath()):
-        """Alias for jsondel (delete the JSON value)."""
+        """
+        Alias for jsondel (delete the JSON value).
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonforget>`_.
+        """
         return self.execute_command("JSON.FORGET", name, str_path(path))
 
     def arrlen(self, name, path=Path.rootPath()):
-        """Return the length of the array JSON value under ``path`` at key``name``."""
+        """
+        Return the length of the array JSON value under `path` at key`name`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonarrlen>`_.
+        """
         return self.execute_command("JSON.ARRLEN", name, str_path(path))
 
     def arrpop(self, name, path=Path.rootPath(), index=-1):
-        """Pop the element at ``index`` in the array JSON value under ``path`` at key ``name``."""
+        """
+        Pop the element at `index` in the array JSON value under `path` at key `name`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonarrpop>`_.
+        """
         return self.execute_command("JSON.ARRPOP", name, str_path(path), index)
 
     def arrtrim(self, name, path, start, stop):
-        """Trim the array JSON value under ``path`` at key ``name`` to the inclusive range given by ``start`` and ``stop``."""
+        """
+        Trim the array JSON value under `path` at key `name` to the inclusive range given by `start` and `stop`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonarrtrim>`_.
+        """
         return self.execute_command("JSON.ARRTRIM", name, str_path(path), start, stop)
 
     def type(self, name, path=Path.rootPath()):
-        """Get the type of the JSON value under ``path`` from key ``name``."""
+        """
+        Get the type of the JSON value under `path` from key `name`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsontype>`_.
+        """
         return self.execute_command("JSON.TYPE", name, str_path(path))
 
     def resp(self, name, path=Path.rootPath()):
-        """Return the JSON value under ``path`` at key ``name``."""
+        """
+        Return the JSON value under `path` at key `name`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonresp>`_.
+        """
         return self.execute_command("JSON.RESP", name, str_path(path))
 
     def objkeys(self, name, path=Path.rootPath()):
-        """Return the key names in the dictionary JSON value under ``path`` at key ``name``."""
+        """
+        Return the key names in the dictionary JSON value under `path` at key `name`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonobjkeys>`_.
+        """
         return self.execute_command("JSON.OBJKEYS", name, str_path(path))
 
     def objlen(self, name, path=Path.rootPath()):
-        """Return the length of the dictionary JSON value under ``path`` at key ``name``."""
+        """
+        Return the length of the dictionary JSON value under `path` at key `name`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonobjlen>`_.
+        """
         return self.execute_command("JSON.OBJLEN", name, str_path(path))
 
     def numincrby(self, name, path, number):
-        """Increment the numeric (integer or floating point) JSON value under ``path`` at key ``name`` by the provided ``number``."""
+        """
+        Increment the numeric (integer or floating point) JSON value under `path` at
+        key `name` by the provided `number`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonnumincrby>`_.
+        """
         return self.execute_command(
             "JSON.NUMINCRBY", name, str_path(path), self._encode(number)
         )
 
     def nummultby(self, name, path, number):
-        """Multiply the numeric (integer or floating point) JSON value under ``path`` at key ``name`` with the provided ``number``."""
+        """
+        Multiply the numeric (integer or floating point) JSON value under `path` at key
+        `name` with the provided `number`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonnummultby>`_.
+        """
         return self.execute_command(
             "JSON.NUMMULTBY", name, str_path(path), self._encode(number)
         )
@@ -76,21 +114,23 @@ class CommandMixin:
     def clear(self, name, path=Path.rootPath()):
         """
         Empty arrays and objects (to have zero slots/keys without deleting the array/object).
-
         Return the count of cleared paths (ignoring non-array and non-objects paths).
         """
         return self.execute_command("JSON.CLEAR", name, str_path(path))
 
     def delete(self, name, path=Path.rootPath()):
-        """Delete the JSON value stored at key ``name`` under ``path``."""
+        """
+        Delete the JSON value stored at key `name` under `path`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsondel>`_.
+        """
         return self.execute_command("JSON.DEL", name, str_path(path))
 
     def get(self, name, *args, no_escape=False):
         """
-        Get the object stored as a JSON value at key ``name``.
-
-        ``args`` is zero or more paths, and defaults to root path
-        ```no_escape`` is a boolean flag to add no_escape option to get non-ascii characters
+        Get the object stored as a JSON value at key `name`.
+        `args` is zero or more paths, and defaults to root path `no_escape` is
+        a boolean flag to add no_escape option to get non-ascii characters.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonget>`_.
         """
         pieces = [name]
         if no_escape:
@@ -111,7 +151,9 @@ class CommandMixin:
             return None
 
     def mget(self, path, *args):
-        """Get the objects stored as a JSON values under ``path`` from keys ``args``."""
+        """
+        Get the objects stored as a JSON values under `path` from keys `args`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonmget>`_."""
         pieces = []
         pieces.extend(args)
         pieces.append(str_path(path))
@@ -119,11 +161,15 @@ class CommandMixin:
 
     def set(self, name, path, obj, nx=False, xx=False, decode_keys=False):
         """
-        Set the JSON value at key ``name`` under the ``path`` to ``obj``.
+        Set the JSON value at key `name` under the `path` to `obj`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonset>`_.
 
-        ``nx`` if set to True, set ``value`` only if it does not exist.
-        ``xx`` if set to True, set ``value`` only if it exists.
-        ``decode_keys`` If set to True, the keys of ``obj`` will be decoded with utf-8.
+        nx : bool
+            If set to True, set `value` only if it does not exist.
+        xx : bool
+            If set to True, set `value` only if it exists.
+        decode_keys : bool
+            If set to True, the keys of `obj` will be decoded with utf-8.
         """
         if decode_keys:
             obj = helpers.decodeDictKeys(obj)
@@ -143,19 +189,30 @@ class CommandMixin:
         return self.execute_command("JSON.SET", *pieces)
 
     def strlen(self, name, path=Path.rootPath()):
-        """Return the length of the string JSON value under ``path`` at key ``name``."""
+        """
+        Return the length of the string JSON value under `path` at key `name`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonstrlen>`_.
+        """
         return self.execute_command("JSON.STRLEN", name, str_path(path))
 
     def toggle(self, name, path=Path.rootPath()):
-        """Toggle boolean value under ``path`` at key ``name``, Returning the new value."""
+        """
+        Toggle boolean value under `path` at key `name`, Returning the new value.
+        """
         return self.execute_command("JSON.TOGGLE", name, str_path(path))
 
     def strappend(self, name, string, path=Path.rootPath()):
-        """Append to the string JSON value under ``path`` at key ``name`` the provided ``string``."""
+        """
+        Append to the string JSON value under `path` at key `name` the provided `string`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsonstrappend>`_.
+        """
         return self.execute_command(
             "JSON.STRAPPEND", name, str_path(path), self._encode(string)
         )
 
     def debug(self, name, path=Path.rootPath()):
-        """Return the memory usage in bytes of a value under ``path`` from key ``name``."""
+        """
+        Return the memory usage in bytes of a value under `path` from key `name`.
+        More information `here <https://oss.redis.com/redisjson/commands/#jsondebug>`_.
+        """
         return self.execute_command("JSON.DEBUG", "MEMORY", name, str_path(path))
